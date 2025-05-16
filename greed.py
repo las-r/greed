c01ad9c79fad4a31436f71598f3b0e7f623444d9
las_r <128936184+las-r@users.noreply.github.com>
2025-05-16 14:35:26 -0400

    Fix ascii error detection and set indents to 4 spaces

--- greed.py
+++ greed.py
@@ -1,7 +1,7 @@
 import sys
 
 # GREED made by las_r on github
-# version 1.1
+# version 1.2
 
 # how to use:
 # put program in 'prog' variable
@@ -22,65 +22,65 @@ mem = False
 # run program
 i = 0
 while i < len(prog):
-  com = prog[i]
-  
-  # FLIP
-  if com == "*":
-    stk[ptr] = not stk[ptr]
+    com = prog[i]
     
-  # TAKE
-  elif com == "(":
-    mem = stk[ptr]
+    # FLIP
+    if com == "*":
+        stk[ptr] = not stk[ptr]
+        
+    # TAKE
+    elif com == "(":
+        mem = stk[ptr]
+        
+    # GIVE
+    elif com == ")":
+        stk[ptr] = mem
+        
+    # FORWARD
+    elif com == ">":
+        ptr += 1
+        try:
+            stk[ptr]
+        except IndexError:
+            stk.append(False)
+            
+    # BACKWARD
+    elif com == "<":
+        ptr -= 1
+        if ptr < 0:
+            stk.insert(0, False)
+            init += 1
+            ptr = 0
+            
+    # REWIND
+    elif com == "&":
+        if mem:
+            i = 0
+            ptr = init
+            
+    # unknown
+    else:
+        print(f"`{com}` is not a known command.")
+        sys.exit(1)
     
-  # GIVE
-  elif com == ")":
-    stk[ptr] = mem
-    
-  # FORWARD
-  elif com == ">":
-    ptr += 1
-    try:
-      stk[ptr]
-    except IndexError:
-      stk.append(False)
-      
-  # BACKWARD
-  elif com == "<":
-    ptr -= 1
-    if ptr < 0:
-      stk.insert(0, False)
-      init += 1
-      ptr = 0
-      
-  # REWIND
-  elif com == "&":
-    if mem:
-      i = 0
-      ptr = init
-      
-  # unknown
-  else:
-    print(f"`{com}` is not a known command.")
-    sys.exit(1)
-  
-  i += 1
+    i += 1
 
 # binary stack
 bstk = ""
 for ind, s in enumerate(stk):
-  if ind - init == 0:
-    bstk += "c"
-  
-  if s:
-    bstk += "1"
-  else:
-    bstk += "0"
+    if ind - init == 0:
+        bstk += "c"
+    
+    if s:
+        bstk += "1"
+    else:
+        bstk += "0"
 
 # ascii stack
-try:
-  astk = ''.join(chr(int(bstk.replace("c", "")[i:i+8], 2)) for i in range(0, len(bstk.replace("c", "")), 8))
-except:
-  astk = None
+if len(bstk.replace("c", "")) % 8 == 0:
+    astk = ''.join(chr(int(bstk.replace("c", "")[i:i+8], 2)) for i in range(0, len(bstk.replace("c", "")), 8))
+else:
+    astk = "Invalid length for ASCII decoding"
 
 # output
 print(f"STACK: {bstk}")
