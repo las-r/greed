3a7d5c9379f0f4174e670a608ab7c300a3b461c2
las_r <128936184+las-r@users.noreply.github.com>
2025-05-19 15:31:43 -0400

    Create binary-converter.py

---
+++ binary-converter.py
@@ -0,0 +1,19 @@
+# binary to greed converter by las_r on github
+
+# usage:
+# input binary into the `binarystr` variable
+# run program in command line
+# copy generated greed program which sets the stack to the binary
+
+# binary thing
+binary = "01001000011001010110110001101100011011110010110000100000011101110110111101110010011011000110010000100001"
+
+# loop through string and convert
+new = ""
+for b in binary:
+    if int(b):
+        new += "*"
+    new += ">"
+
+# output program
+print(new)
